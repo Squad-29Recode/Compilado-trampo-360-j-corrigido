e4c0456381a4d0c1d6932dd9ac48915f90a6adf0
DENISE FERNANDA <140545608+Dnss777@users.noreply.github.com>
2023-11-17 10:39:20 -0300

    1 parte do trabalho

---
+++ java/dao/AquisicaoDAO.java
@@ -0,0 +1,223 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import connection.ConnectionMySQL;
+import model.Aquisicao;
+
+public class AquisicaoDAO {
+	
+	//CREATE
+	public void creat (Aquisicao aquisicao) {
+		String sql = "insert into Aquisicao (data_Aquisicao, nome, id_Evento, id_Aluno) values (?, ?, ?, ?)";
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		
+		try {
+			conn = ConnectionMySQL.createConnectionMySQL();
+			pstm = conn.prepareStatement(sql);
+			
+			pstm.setString(1, aquisicao.getdata_Aquisicao());
+			pstm.setString(2, aquisicao.getNome());
+			pstm.setInt(3, aquisicao.getid_Evento());
+			pstm.setInt(4, aquisicao.getId_Aluno());
+			
+			pstm.execute();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		}finally {
+			 try {
+				
+				 if (pstm !=null) {
+					 pstm.close();
+				
+				 }
+				 if (conn !=null) {
+					 conn.close();
+				 }
+				 
+				 }catch (Exception e) {
+					 e.printStackTrace();
+				 }
+	}
+		
+	}
+
+	//READ
+		public List <Aquisicao> read(){
+			List<Aquisicao> aquisicaos = new ArrayList<Aquisicao>();
+			String sql = "select * from aquisicao";
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		ResultSet rset = null;
+		
+		try {
+			conn = ConnectionMySQL.createConnectionMySQL();
+			pstm = conn.prepareStatement(sql);
+			
+			rset = pstm.executeQuery();
+			
+			while (rset.next()) {
+				Aquisicao aquisicao = new Aquisicao();
+				aquisicao.setdata_Aquisicao (rset.getString("data_Aquisicao"));
+				aquisicao.setNome(rset.getString("nome"));
+				aquisicao.setid_Evento(rset.getInt("id_Evento"));
+				aquisicao.setId_Aluno(rset.getInt("id_Aluno"));
+				
+				((List<Aquisicao>) aquisicao).add(aquisicao);
+			}
+		
+	} catch (Exception e) {
+		e.printStackTrace();
+		
+	}finally {
+		 try {
+			
+			 if (pstm !=null) {
+				 pstm.close();
+			
+			 }
+			 if (conn !=null) {
+				 conn.close();
+			 }
+			 
+			 }catch (Exception e) {
+				 e.printStackTrace();
+			 }
+}
+		
+		return aquisicaos;
+	}
+	
+	//UPDATE
+	public void update (Aquisicao aquisicao) {
+		String sql = "UPDATE aquisicao SET data_Aquisicao = ?,  nome = ?, id_Evento = ?, id_Aluno = ? WHERE id_Aquisicao = ?";
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		
+		try {
+			conn = ConnectionMySQL.createConnectionMySQL();
+			
+			pstm = conn.prepareStatement(sql);
+			
+			pstm.setString(1, aquisicao.getdata_Aquisicao());
+			pstm.setString(2, aquisicao.getNome());
+			pstm.setInt(3, aquisicao.getid_Evento());
+			pstm.setInt(4, aquisicao.getId_Aluno());
+			
+			pstm.setInt(5, aquisicao.getId_Aquisicao());
+			
+			pstm.execute();
+		
+	} catch (Exception e) {
+		e.printStackTrace();
+		
+	}finally {
+		 try {
+			
+			 if (pstm !=null) {
+				 pstm.close();
+			
+			 }
+			 if (conn !=null) {
+				 conn.close();
+			 }
+			 
+			 }catch (Exception e) {
+				 e.printStackTrace();
+			 }
+}
+	} 
+	
+	//DELETE
+	public static void delete (int id_Aquisicao) {
+	String sql = "DELETE FROM aquisicao WHERE id_Aquisicao = ?";
+	
+	Connection conn = null;
+	PreparedStatement pstm = null;
+	
+	try {
+		conn = ConnectionMySQL.createConnectionMySQL();
+		
+		pstm = conn.prepareStatement(sql);
+		
+		pstm.setInt(1, id_Aquisicao);
+		
+		pstm.execute();
+	
+	} catch (Exception e) {
+		e.printStackTrace();
+		
+	}finally {
+		 try {
+			
+			 if (pstm !=null) {
+				 pstm.close();
+			
+			 }
+			 if (conn !=null) {
+				 conn.close();
+			 }
+			 
+			 }catch (Exception e) {
+				 e.printStackTrace();
+			 
+}
+	}
+	}
+	//readById
+	public Aquisicao readById (int id_Aquisicao) {
+		Aquisicao aquisicao = new Aquisicao();
+		String sql = "select * from aquisicao WHERE id_Aquisicao= ?";
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		ResultSet rset = null;
+		
+		try {
+			conn = ConnectionMySQL.createConnectionMySQL();
+			
+			pstm = conn.prepareStatement(sql);
+			
+			pstm.setInt(1, id_Aquisicao);
+			
+			rset = pstm.executeQuery();
+			
+			rset.next();
+			
+			aquisicao.setdata_Aquisicao (rset.getString("data_Aquisicao"));
+			aquisicao.setNome (rset.getString("nome"));
+			aquisicao.setid_Evento (rset.getInt("id_Evento"));
+			aquisicao.setId_Aluno (rset.getInt("id_Aluno"));
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		}finally {
+			
+			try {
+				 if (pstm !=null) {
+					 pstm.close();
+				
+				 }
+				 if (conn !=null) {
+					 conn.close();
+				 }
+				 
+				 }catch (Exception e) {
+					 e.printStackTrace();
+				 }
+	    }
+		return aquisicao;
+		}
+	}
+
